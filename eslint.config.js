f89aa54966982d25a853e64e9455a9b9836849da
Nello-Moreira <g.moreira1@outlook.com>
2023-12-29 11:47:32 -0300

    Config: Fix eslint to typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,30 @@
+/* eslint-disable */
+const js = require('@eslint/js');
 const tsPlugin = require('@typescript-eslint/eslint-plugin');
 const tsParser = require('@typescript-eslint/parser');
 const eslintConfigPrettier = require('eslint-config-prettier');
+const globals = require('globals');
 
 module.exports = [
-	'eslint:recommended',
 	eslintConfigPrettier,
-	{ rules: tsPlugin.configs['eslint-recommended'].overrides[0].rules },
-	{ rules: tsPlugin.configs.recommended.rules },
+	{
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: { project: ['tsconfig.json'] },
+		},
+		plugins: { '@typescript-eslint': tsPlugin },
+		rules: {
+			...js.configs.recommended.rules,
+			...tsPlugin.configs.all.rules,
+			'@typescript-eslint/consistent-type-definitions': 'off',
+			'@typescript-eslint/prefer-readonly-parameter-types': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+		},
+	},
 	{
 		files: ['**/*.ts'],
-		languageOptions: { parser: tsParser },
-		plugins: { '@typescript-eslint': tsPlugin },
+		languageOptions: {
+			globals: { ...globals.node, myCustomGlobal: 'readonly' },
+		},
 	},
 ];
